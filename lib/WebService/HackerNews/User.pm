7f0e9a642792e958467875277db879149df1d3f1
Neil Bowers <neil@bowers.com>
2014-10-09 21:50:07 +0000

    Import of NEILB/WebService-HackerNews-0.01 from CPAN.
    gitpan-cpan-distribution: WebService-HackerNews
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         NEILB/WebService-HackerNews-0.01.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

---
+++ lib/WebService/HackerNews/User.pm
@@ -0,0 +1,12 @@
+package WebService::HackerNews::User;
+$WebService::HackerNews::User::VERSION = '0.01';
+use Moo;
+
+has id        => (is => 'ro');
+has delay     => (is => 'ro');
+has created   => (is => 'ro');
+has karma     => (is => 'ro');
+has about     => (is => 'ro');
+has submitted => (is => 'ro');
+
+1;
